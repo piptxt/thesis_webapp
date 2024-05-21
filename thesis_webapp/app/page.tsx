343a455e2957dc082fef24d73639d229d40569c1
piptxt <andres_clemente@dlsu.edu.ph>
2024-05-21 23:15:49 +0800

    connected to mongodb
    with display functionality with no styling

--- thesis_webapp/app/page.tsx
+++ thesis_webapp/app/page.tsx
@@ -1,16 +1,34 @@
 import Image from "next/image";
 import TopBar from "./Components/TopBar/TopBar.jsx";
 import SearchBar from "./Components/SearchBar/SearchBar.jsx";
-import DocumentResults from "./Components/DocumentResults/DocumentResults.jsx";
+import DocumentResults from "./Components/DocumentResults/DocumentResults";
 import NavBar from "./Components/NavBar/NavBar.jsx";
+import clientPromise from "../lib/mongodb";
 
+export default async function Home() {
+  const client = await clientPromise;
+  const db = client.db("sample_mflix");
+  const movies = await db
+    .collection("movies")
+    .find({})
+    .sort({ metacritic: -1 })
+    .limit(20)
+    .toArray();
 
-export default function Home() {
   return (
     <main>
       {/* <TopBar /> */}
       <NavBar />
       <SearchBar />
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie._id}>
+            <h2>{movie.title}</h2>
+            <h3>{movie.metacritic}</h3>
+            <p>{movie.plot}</p>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }

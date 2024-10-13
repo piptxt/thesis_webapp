eddc2a2991a9df14bacc38cab03d211b1b668cad
Priscilla Licup <priscillalicup@gmail.com>
2024-10-13 16:40:41 +0800

    Update Vector Search
    - fix routing (exclude splitting chunks route)
    - fix python function for vector search results
    - exclude chunks display on Vector Search Results page

--- thesis_webapp/pages/api/advance_search.ts
+++ thesis_webapp/pages/api/advance_search.ts
@@ -1,6 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
 
+// This file is for the routing of Vector Search
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     if (req.method !== 'POST') {
@@ -15,20 +17,31 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }
 
     let response;
-    let response_query;
 
-    if (initial === true) {
-      response_query = await axios.post("http://localhost:5000/split_to_chunks", { text: query });
-      response = await axios.post("http://localhost:5000/vector_results", { text: response_query.data[0], category });
-    } else {
-      response = await axios.post("http://localhost:5000/vector_results", { text: query, category });
-      response_query = await axios.post("http://localhost:5000/split_to_chunks", { text: query });
-    }
+    // FOR CHUNKS ---------------
+    // let response_query;
 
-    if (response && response_query) {
+    // if (initial === true) {
+    //   response_query = await axios.post("http://localhost:5000/split_to_chunks", { text: query });
+    //   response = await axios.post("http://localhost:5000/vector_results", { text: response_query.data[0], category });
+    // } else {
+    //   response = await axios.post("http://localhost:5000/vector_results", { text: query, category });
+    //   response_query = await axios.post("http://localhost:5000/split_to_chunks", { text: query });
+    // }
+
+    response = await axios.post("http://localhost:5000/vector_results", { text: query, category });
+
+
+    // FOR CHUNKS ---------------
+    // if (response && response_query) {
+    //   const documents = response.data;
+    //   const query_chunks = response_query.data;
+    //   res.status(200).json({ documents, query_chunks, category });
+    // }
+
+    if (response) {
       const documents = response.data;
-      const query_chunks = response_query.data;
-      res.status(200).json({ documents, query_chunks, category });
+      res.status(200).json({ documents, category });
     }
   } catch (error) {
     console.error(error);
